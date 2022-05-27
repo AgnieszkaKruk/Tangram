f88e72a8ccd75c2305cc44e5f8371ca08aa3fd6a
NataliaGol <nataliahreast@gmail.com>
2022-05-28 00:00:11 +0200

    Add timer to start_game page, and confetti after win

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-  
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route('/start_game')
 def start_game():
     return render_template('start_game.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
-
 
 
 @app.route('/register')
@@ -34,6 +33,5 @@ def ranking():
     return render_template('ranking.html')
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
